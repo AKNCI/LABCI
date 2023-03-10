c5b8da76a33e676e63557803cdb451ab580f30fe
Poco90 <116454013+Poco90@users.noreply.github.com>
2023-03-10 16:13:46 +0000

    added continous delivery

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js wth a built-in load balancer.
 sudo npm install -g pm2
 #stop any instance of our application running currently
-pm2 stop example app
+pm2 stop example_app
 # change directory into folder where application is downloaed
-cd LabCI/
+cd LABCI/
 #Install application dependecies
 npm install
 #start the application withthe process name example_app using pm2
